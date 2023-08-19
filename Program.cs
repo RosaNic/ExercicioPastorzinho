f6290a1ecadbcb48329c039f129b2990ea49972c
Nicole <rosanik3103@gmail.com>
2023-08-19 00:43:05 -0300

    Toca a musica Pastorzinho

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿#pragma warning disable CA1416
+
+Console.Beep(1320, 300); Console.Write("Dó,");
+Console.Beep(1485, 300); Console.Write("Ré,");
+Console.Beep(1650, 300); Console.Write("Mi,");
+Console.Beep(1759, 400); Console.Write("Fá,");
+Console.Beep(1759, 400); Console.Write("Fá,");
+Console.Beep(1759, 400); Console.WriteLine("Fá,");
+Console.Beep(1320, 300); Console.Write("Dó,");
+Console.Beep(1485, 300); Console.Write("Ré,");
+Console.Beep(1320, 300); Console.Write("Dó,");
+Console.Beep(1485, 400); Console.Write("Ré,");
+Console.Beep(1485, 400); Console.Write("Ré,");
+Console.Beep(1485, 400); Console.WriteLine("Ré,");
+Console.Beep(1320, 300); Console.Write("Dó,");
+Console.Beep(1980, 300); Console.Write("Sol,");
+Console.Beep(1759, 400); Console.Write("Fá,");
+Console.Beep(1650, 300); Console.Write("Mi,");
+Console.Beep(1650, 300); Console.Write("Mi,");
+Console.Beep(1650, 300); Console.WriteLine("Mi,");
+Console.Beep(1320, 300); Console.Write("Dó,");
+Console.Beep(1485, 400); Console.Write("Ré,");
+Console.Beep(1650, 300); Console.Write("Mi,");
+Console.Beep(1759, 400); Console.Write("Fá,");
+Console.Beep(1759, 400); Console.Write("Fá,");
+Console.Beep(1759, 400); Console.Write("Fá,");
